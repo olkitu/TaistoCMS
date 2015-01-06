81cb4a89316e240bbeaa3702fcefccb50c4a5d84
Minh Truong <olkitu@users.noreply.github.com>
2015-01-07 00:21:01 +0200

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,9 @@
+--
+-- Table structure for table `Website`
+--
+CREATE TABLE IF NOT EXISTS `website` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
+  `content` text CHARACTER SET utf8 COLLATE utf8_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
